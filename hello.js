9a471ae10a8306285715d0e747bfe9e2c5d2d6db
Abhishek <abhi.ghosh27feb@gmail.com>
2021-12-28 20:51:23 +0530

    first commit

---
+++ hello.js
@@ -0,0 +1,36 @@
+// console.log("hello")
+// //declaration of variable
+// //es5 way of declaration of variable
+// var a=0;
+// var b=2;
+// console.log("a",a)
+// console.log("b",b)
+// // operators in java script arthmetic operator
+// //+ - / * **
+
+// var x=Number(prompt("enter first no"))
+// var y=Number(prompt("enter second no"))
+// //+
+// console.log("x+y",x+y)
+// console.log("x-y",x-y)
+// console.log("x*y",x*y)
+// console.log("x**y",x**y)
+//conditional oprators
+//> < >-= <= == ===
+// == ===
+// var v=1
+// var w='1'
+// console.log("v==w",v==w)
+// console.log("v===w",v===w)
+// // what is the defrence between null and undifined
+// var k=null
+// console.log("k",k)
+// console.log("type of k",typeof(k))
+// var j
+// console.log("type of j",typeof(j))
+if(2>3){
+    console.log("bye")
+}
+else{
+    console.log("hello")
+}
